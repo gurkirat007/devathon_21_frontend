cc4e911901c6786f95a2aab45130d376fe1cfd1d
vatsalkumar24 <83907433+vatsalkumar24@users.noreply.github.com>
2021-09-17 22:48:44 +0300

    ok 6

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,25 +1,51 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-class Navbar extends Component {
+class Landing extends Component {
   render() {
     return (
-      <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper white">
-            <Link
-              to="/"
-              style={{
-                fontFamily: "monospace"
-              }}
-              className="col s5 brand-logo center black-text"
-            >
-              <i className="material-icons">code</i>
-              MERN
-            </Link>
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Build</b> a login/auth app with the{" "}
+              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
+              scratch
+            </h4>
+            <p className="flow-text grey-text text-darken-1">
+              Create a (minimal) full-stack app with user authentication via
+              passport and JWTs
+            </p>
+            <br />
+            <div className="col s6">
+              <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Register
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link
+                to="/login"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large btn-flat waves-effect white black-text"
+              >
+                Log In
+              </Link>
+            </div>
           </div>
-        </nav>
+        </div>
       </div>
     );
   }
 }
-export default Navbar;
+export default Landing;
