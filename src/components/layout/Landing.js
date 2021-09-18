835588eb9cc91d0b1ec16f4de1ac0cc139611bfe
Gurkirat Singh <gurkiratsingh@pop-os.localdomain>
2021-09-18 18:58:49 +0530

    added search bar

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import SearchBar from "./search";
+
 class Landing extends Component {
   render() {
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
+            <SearchBar />
           </div>
         </div>
       </div>
---
+++ src/components/layout/search.js
@@ -0,0 +1,12 @@
+import React from "react";
+const SearchBar = () => (
+  <form action="/" method="get">
+    <label htmlFor="header-search">
+      <span className="visually-hidden">Names</span>
+    </label>
+    <input type="text" id="header-search" placeholder="Find Prof" name="s" />
+    <button type="submit">Search</button>
+  </form>
+);
+
+export default SearchBar;
